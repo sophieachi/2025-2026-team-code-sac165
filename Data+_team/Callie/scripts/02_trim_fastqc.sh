c3cca653cac0b6b8495ef60c9bb7e1b68ba27334
Calista Hundley <clh162@dcc-login-04.oit.duke.edu>
2026-05-20 10:00:55 -0400

    fixing definition of sample name

--- Data+_team/Callie/scripts/02_trim_fastqc.sh
+++ Data+_team/Callie/scripts/02_trim_fastqc.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=trim_fastqc_array
 #SBATCH --time=7-00:00:00
 #SBATCH --array=1-36
-#SBATCH --output=logs/trim_fastqc_%a.out
-#SBATCH --error=logs/trim_fastqc_%a.err
+#SBATCH --output=/work/clh162/OysterRNA24/logs/trim_fastqc_%a.out
+#SBATCH --error=/work/clh162/OysterRNA24/logs/trim_fastqc_%a.err
 #SBATCH --partition=common
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=8
@@ -20,14 +20,14 @@ FASTQC_OUT=/work/clh162/OysterRNA24/fastqc_trimmed
 mkdir -p $FASTQC_OUT
 
 ## Set up direction/path to each sample ##
-SAMPLES=($(ls ${RAW_DIR}/*_R1_001.fastq.gz | sed 's/_R1_001.fastq.gz//' | xargs -n 1 basename))
+SAMPLES=($(ls ${TRIMMED_DIR}/*_R1_001_val_1.fq.gz | sed 's/_R1_001_val_1.fq.gz//' | xargs -n 1 basename))
 
 # Index an individual sample from the list for this array task
 SAMPLE=${SAMPLES[$SLURM_ARRAY_TASK_ID-1]}
 
 # Define R1 and R2 for the sample 
-R1=${RAW_DIR}/${SAMPLE}_R1_001.fastq.gz
-R2=${RAW_DIR}/${SAMPLE}_R2_001.fastq.gz
+R1=${TRIMMED_DIR}/${SAMPLE}_R1_001_val_1.fq.gz
+R2=${TRIMMED_DIR}/${SAMPLE}_R1_001_val_1.fq.gz
 
 
 ## Run FastQC on Trimmed Samples ##
